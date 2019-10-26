b74922464f04ff1fc6087f9043a41492be5dfffd
lillycoder <ltheinh@gmail.com>
2019-10-26 19:50:07 +0000

    added testing

--- test/controllers/owner/tenant_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Owner::TenantControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/controllers/owner/tenants_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Owner::TenantsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
